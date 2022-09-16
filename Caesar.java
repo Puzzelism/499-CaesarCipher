6b9f0659e58904161d7f7126bc4d151c256eaae4
Baillie Stang <baillie.stang@gmail.com>
2022-09-16 12:36:04 -0700

    Create Caesar.java
    Create master file and begin I/O structure

---
+++ Caesar.java
@@ -0,0 +1,26 @@
+// Baillie Stang
+// 2022
+
+import java.util.Scanner;
+
+public class Caesar {
+    public static void main(String args[]){
+
+        // variable declaration
+        Scanner scan = new Scanner(System.in);        
+        String input;
+
+        // get input
+        System.out.println("Enter text to encrypt: ");
+        input= scan.nextLine();
+
+        /*
+         * 
+         * Advanced algorithms :o
+         * 
+         */
+
+        // output
+        System.out.println(input);
+    }
+}
